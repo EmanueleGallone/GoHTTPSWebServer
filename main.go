9a63b3d1a3f7b98835a249a14454e07be4d43ee2
unknown <emanuele.gallone@mail.polimi.it>
2019-01-27 00:54:17 +0100

    added template example

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"./cert"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-var listenAddr = "127.0.0.1"
-var httpsPort = ":9090"
+const listenAddr = "127.0.0.1"
+const httpsPort = ":9090"
+var httpsAddr = listenAddr+httpsPort
 
 func main() {
-
-	httpsAddr := listenAddr+httpsPort
 
 	// Check if the cert files are available.
 	// If they are not available, generate new ones.
@@ -28,19 +28,32 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	//start HTTPS on Goroutine
-	go http.ListenAndServeTLS(httpsAddr, "cert.pem", "key.pem", nil)
+	go HTTPS()
 
 	// Start the HTTP server and redirect all incoming connections to HTTPS
 	err3 := http.ListenAndServe(listenAddr+":80", http.HandlerFunc(redirectToHttps))
 	if err3 != nil {
-		fmt.Println("Error:",err3)
+		log.Println("Error:",err3)
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if _,err :=fmt.Fprintf(w, "Hi there! from HTTPS!"); err != nil {
-		fmt.Println("Error:",err)
+	//if _,err :=fmt.Fprintf(w, "Hi there! from HTTPS!"); err != nil {
+	//	log.Println("Error:",err)
+	//}
+
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, "Hello World!")
+}
+
+func HTTPS() {
+	log.Println("Starting Https Server on " + httpsAddr)
+	if err := http.ListenAndServeTLS(httpsAddr, "cert.pem", "key.pem", nil); err != nil {
+		log.Fatal(err)
+		return
 	}
+
+
 }
 
 func redirectToHttps(w http.ResponseWriter, r *http.Request) {
